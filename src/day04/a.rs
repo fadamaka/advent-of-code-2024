5b3fcd34ea9c2031402ff66e532ba424a5dfc52f
Tamas Hercz <tamas.hercz@cubicfox.com>
2024-12-04 07:46:11 +0100

    day04 done

--- src/day04/a.rs
+++ src/day04/a.rs
@@ -3,11 +3,99 @@ use advent_of_code_2024::utils::util::read_lines;
 pub fn run(input_path: &str) -> i32 {
     let mut result: i32 = 0;
     if let Ok(lines) = read_lines(input_path) {
-        for line in lines.flatten() {
-            println!("{}", line);
+        let lines_v = lines.flatten().collect::<Vec<_>>();
+        for i in 0..lines_v.len() {
+            let line = lines_v.get(i).unwrap();
+            for j in 0..line.len() {
+                if line.chars().nth(j).unwrap() == 'X' {
+                    if find_ch_h(i, j, lines_v.to_owned(), "XMAS") {
+                        result += 1;
+                    }
+                    if find_ch_v(i, j, lines_v.to_owned(), "XMAS") {
+                        result += 1;
+                    }
+                    if find_ch_d(i, j, lines_v.to_owned(), "XMAS") {
+                        result += 1;
+                    }
+                    if find_ch_d2(i, j, lines_v.to_owned(), "XMAS") {
+                        result += 1;
+                    }
+                }
+                if line.chars().nth(j).unwrap() == 'S' {
+                    if find_ch_h(i, j, lines_v.to_owned(), "SAMX") {
+                        result += 1;
+                    }
+                    if find_ch_v(i, j, lines_v.to_owned(), "SAMX") {
+                        result += 1;
+                    }
+                    if find_ch_d(i, j, lines_v.to_owned(), "SAMX") {
+                        result += 1;
+                    }
+                    if find_ch_d2(i, j, lines_v.to_owned(), "SAMX") {
+                        result += 1;
+                    }
+                }
+            }
         }
     }
     result
+}
+
+fn find_ch_h(i: usize, j: usize, lines: Vec<String>, target: &str) -> bool {
+    let line = lines.get(i).unwrap();
+    let target_chars: Vec<_> = target.chars().collect();
+    let chars: Vec<_> = line.chars().collect();
+    for n in 0..target.len() {
+        if target_chars.get(0 + n).is_none() {
+            println!("WTF");
+        }
+        if chars.get(j + n).unwrap_or(&'B') != target_chars.get(n).unwrap() {
+            return false;
+        }
+    }
+    true
+}
+fn find_ch_v(i: usize, j: usize, lines: Vec<String>, target: &str) -> bool {
+    let target_chars: Vec<_> = target.chars().collect();
+    for n in 0..target.len() {
+        let chars: Vec<_> = lines
+            .get(i + n)
+            .unwrap_or(&String::from("NOT IT"))
+            .chars()
+            .collect();
+        if chars.get(j).unwrap_or(&'B') != target_chars.get(n).unwrap() {
+            return false;
+        }
+    }
+    true
+}
+fn find_ch_d(i: usize, j: usize, lines: Vec<String>, target: &str) -> bool {
+    let target_chars: Vec<_> = target.chars().collect();
+    for n in 0..target.len() {
+        let chars: Vec<_> = lines
+            .get(i + n)
+            .unwrap_or(&String::from("NOT IT"))
+            .chars()
+            .collect();
+        if chars.get(j + n).unwrap_or(&'B') != target_chars.get(n).unwrap() {
+            return false;
+        }
+    }
+    true
+}
+fn find_ch_d2(i: usize, j: usize, lines: Vec<String>, target: &str) -> bool {
+    let target_chars: Vec<_> = target.chars().collect();
+    for n in 0..target.len() {
+        let chars: Vec<_> = lines
+            .get(i + n)
+            .unwrap_or(&String::from("NOT IT"))
+            .chars()
+            .collect();
+        if j < n || chars.get(j - n).unwrap_or(&'B') != target_chars.get(n).unwrap() {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -15,10 +103,10 @@ mod runs {
     use super::*;
     #[test]
     fn testinput() {
-        assert_eq!(run("./src/day02/testinput.txt"), 0);
+        assert_eq!(run("./src/day04/testinput.txt"), 18);
     }
     #[test]
     fn input() {
-        assert_eq!(run("./src/day02/input.txt"), 0);
+        assert_eq!(run("./src/day04/input.txt"), 2567);
     }
 }
--- src/day04/b.rs
+++ src/day04/b.rs
@@ -3,11 +3,64 @@ use advent_of_code_2024::utils::util::read_lines;
 pub fn run(input_path: &str) -> i32 {
     let mut result: i32 = 0;
     if let Ok(lines) = read_lines(input_path) {
-        for line in lines.flatten() {
-            println!("{}", line);
+        let lines_v = lines.flatten().collect::<Vec<_>>();
+        for i in 0..lines_v.len() {
+            let line = lines_v.get(i).unwrap();
+            for j in 0..line.len() {
+                if line.chars().nth(j).unwrap() == 'A' {
+                    if find_x(i, j, lines_v.to_owned()) {
+                        result += 1;
+                    }
+                }
+            }
         }
     }
     result
+}
+
+fn find_x(i: usize, j: usize, lines: Vec<String>) -> bool {
+    let mut axis_1: bool = false;
+    let mut axis_2: bool = false;
+    if j == 0 || i == 0 {
+        return false;
+    }
+    let above_line: Vec<_> = lines
+        .get(i - 1)
+        .unwrap_or(&String::from("NOT IT"))
+        .chars()
+        .collect();
+    let below_line: Vec<_> = lines
+        .get(i + 1)
+        .unwrap_or(&String::from("NOT IT"))
+        .chars()
+        .collect();
+    if
+        above_line.get(j - 1).unwrap_or(&'B') == &'M' &&
+        below_line.get(j + 1).unwrap_or(&'B') == &'S'
+    {
+        axis_1 = true;
+    }
+    if
+        above_line.get(j - 1).unwrap_or(&'B') == &'S' &&
+        below_line.get(j + 1).unwrap_or(&'B') == &'M'
+    {
+        axis_1 = true;
+    }
+
+    if
+        above_line.get(j + 1).unwrap_or(&'B') == &'M' &&
+        below_line.get(j - 1).unwrap_or(&'B') == &'S'
+    {
+        axis_2 = true;
+    }
+    if
+        above_line.get(j + 1).unwrap_or(&'B') == &'S' &&
+        below_line.get(j - 1).unwrap_or(&'B') == &'M'
+    {
+        axis_2 = true;
+    }
+
+    axis_1 && axis_2
 }
 
 #[cfg(test)]
@@ -15,10 +68,10 @@ mod runs {
     use super::*;
     #[test]
     fn testinput() {
-        assert_eq!(run("./src/day02/testinput.txt"), 0);
+        assert_eq!(run("./src/day04/testinput.txt"), 9);
     }
     #[test]
     fn input() {
-        assert_eq!(run("./src/day02/input.txt"), 0);
+        assert_eq!(run("./src/day04/input.txt"), 2029);
     }
 }
